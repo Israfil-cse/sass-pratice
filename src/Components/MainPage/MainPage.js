f69c77836838dbf4ef8ab40cf1fc630dc1eb271d
israfil-cse <israfilhossen10750@gmail.com>
2022-01-18 19:32:47 +0600

    [ADD]sass-variable

---
+++ src/Components/MainPage/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../../sass/mainPage.scss";
+
+const MainPage = () => {
+    return (
+        <div className="main_content">
+            <h2>Welcome to our website</h2>
+            <button>read more</button>
+        </div>
+    );
+};
+
+export default MainPage;
---
+++ src/Components/Navber/Navber.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../../sass/nav.scss";
+
+const Navber = () => {
+    return (
+        <div className="navber">
+            <a href="/home">Home</a>
+            <a href="/about">learn sass</a>
+            <a href="/sass">advance sass</a>
+        </div>
+    );
+};
+
+export default Navber;
