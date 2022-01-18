7130f5e828bdacd92b502cc47d8d7fc76dc818f1
israfil-cse <israfilhossen10750@gmail.com>
2022-01-18 19:53:00 +0600

    [ADD]nested style

--- src/Components/Navber/Navber.js
+++ src/Components/Navber/Navber.js
@@ -5,8 +5,8 @@ const Navber = () => {
     return (
         <div className="navber">
             <a href="/home">Home</a>
-            <a href="/about">learn sass</a>
-            <a href="/sass">advance sass</a>
+            <a href="/about">learn <span>sass</span></a>
+            <a href="/sass">advance <span>sass</span></a>
         </div>
     );
 };
